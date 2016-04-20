1aad5c30e563695029432cc35573946003c83f84
aehernandez <alainh@seas.upenn.edu>
2016-04-19 21:43:32 -0400

    added docs for things that will probably end up facing the user

--- rump/client/sidebar-items.js
+++ rump/client/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"enum":[["SessionState",""]],"struct":[["Client",""],["Session",""]]});
+initSidebarItems({"enum":[["SessionState",""]],"struct":[["Client","Client is struct which has methods for creating a new WAMP Session."],["Session","A Session represents a valid WAMP Session with a Router. You can obtain a `Session` from a `Client`"]]});
