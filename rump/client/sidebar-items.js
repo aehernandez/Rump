074dad6e1d1389d964ff7c80036c7efd34bd9344
aehernandez <alainh@seas.upenn.edu>
2016-04-19 22:10:11 -0400

    fixed doc formatting

--- rump/client/sidebar-items.js
+++ rump/client/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"enum":[["SessionState",""]],"struct":[["Client","Client is struct which has methods for creating a new WAMP Session."],["Session","A Session represents a valid WAMP Session with a Router. You can obtain a `Session` from a `Client`"]]});
+initSidebarItems({"enum":[["SessionState",""]],"struct":[["Client","A Client defines methods and options for building a Session with a WAMP Router"],["Session","A Session represents a valid WAMP Session with a Router. You can obtain a `Session` from a `Client`"]]});
