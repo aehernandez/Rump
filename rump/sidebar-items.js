c327b5538ae9a3f8b595eed7e91da1fc2dece23b
aehernandez <alainh@seas.upenn.edu>
2016-04-25 00:21:13 -0400

    updated docs. made index page have public facing docs. will add full docs in a subfolder

--- rump/sidebar-items.js
+++ rump/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"enum":[["WampError",""]],"mod":[["client",""],["message",""],["options",""],["transport",""]],"type":[["WampResult",""]]});
+initSidebarItems({"enum":[["WampError",""],["WampType","All types that can be sent to/from a WAMP Router Used to publish hetereogenous arguments"]],"mod":[["client",""]],"struct":[["Payload","A struct representing the payload that's received from a WAMP event."]],"type":[["WampResult",""]]});
